df6a66a6de5da3537da3ee0ae639cf6717088455
Philipp Strauß <phil13.strauss@gmail.com>
2022-05-18 11:27:59 +0200

    add getSingleUser/Order by id, create controllers+routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,4 +4,18 @@ export const getAllUsers = (req, res) => {
     pool.query("SELECT * FROM users")
     .then(data => res.status(200).json({users: data.rows}))
     .catch(err => res.status(500).json(err))
-}
+};
+
+
+export const getSingleUser = (req, res) => {
+    const { id } = req.params
+    pool.query("SELECT * FROM users WHERE id=$1", [id])
+    .then(data => {
+        if (data.rowCount == 0) {
+            res.status(404).send("No user found matching this id.")
+        } else {
+            res.status(200).json(data.rows[0])
+        }
+    })
+    .catch(err => res.status(500).json(err));
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { getAllUsers } from "../controllers/usersController.js";
-
+import { getSingleUser } from "../controllers/usersController.js";
 const usersRouter = Router();
 
-usersRouter.route("/").get(getAllUsers)
+usersRouter.route("/").get(getAllUsers);
 
 
-usersRouter.route("/:id")
+usersRouter.route("/:id").get(getSingleUser);
 
 
 
