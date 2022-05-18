ad11762410e59cdea382d341f4c675b0541c1f45
Philipp Strauß <phil13.strauss@gmail.com>
2022-05-18 10:55:49 +0200

    add getAllUsers/Orders in respective controllers+routes

---
+++ controllers/usersController.js
@@ -0,0 +1,7 @@
+import pool from "../database/pg.js";
+
+export const getAllUsers = (req, res) => {
+    pool.query("SELECT * FROM users")
+    .then(data => res.status(200).json({users: data.rows}))
+    .catch(err => res.status(500).json(err))
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/usersController.js";
 
 const usersRouter = Router();
+
+usersRouter.route("/").get(getAllUsers)
+
+
+usersRouter.route("/:id")
+
+
 
 export default usersRouter;
